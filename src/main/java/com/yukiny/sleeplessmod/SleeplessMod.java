7e42c1c058f87aba7d11dcbcd39d278d934d121e
yuki <yukiny0811@gmail.com>
2020-06-09 04:23:15 +0900

    Changed Mod Name

--- src/main/java/com/yukiny/sleeplessmod/SleeplessMod.java
+++ src/main/java/com/yukiny/sleeplessmod/SleeplessMod.java
@@ -1,14 +1,14 @@
-package com.example.examplemod;
+package com.yukiny.sleeplessmod;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = SleeplessMod.MODID, version = SleeplessMod.VERSION)
+public class SleeplessMod
 {
-    public static final String MODID = "examplemod";
+    public static final String MODID = "sleeplessmod";
     public static final String VERSION = "1.0";
     
     @EventHandler
