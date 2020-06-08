d51f1cec73600d1e023fcd9e20531cbe5e719ea3
yuki <yukiny0811@gmail.com>
2020-06-09 04:34:51 +0900

    fixed mod settings, and added EventHandlers to SleeplessMod.java

--- src/main/java/com/yukiny/sleeplessmod/SleeplessMod.java
+++ src/main/java/com/yukiny/sleeplessmod/SleeplessMod.java
@@ -1,20 +1,35 @@
 package com.yukiny.sleeplessmod;
 
-import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 
 @Mod(modid = SleeplessMod.MODID, version = SleeplessMod.VERSION)
 public class SleeplessMod
 {
     public static final String MODID = "sleeplessmod";
     public static final String VERSION = "1.0";
-    
+
     @EventHandler
-    public void init(FMLInitializationEvent event)
-    {
-        // some example code
-        System.out.println("DIRT BLOCK >> "+Blocks.DIRT.getUnlocalizedName());
+    public void preinit(FMLPreInitializationEvent event){
+
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+
+    }
+
+    @EventHandler
+    public void postinit(FMLPostInitializationEvent event){
+
+    }
+
+    @EventHandler
+    public void serverStarting(FMLServerStartingEvent event){
+
     }
 }
